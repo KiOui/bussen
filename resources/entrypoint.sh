19b6b21833d1cca14458f3bcd980f4d09a46c32c
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-11-14 15:27:49 +0100

    ASGI docker

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -15,20 +15,10 @@ cd /games/src/website
 
 chown --recursive www-data:www-data /games/
 
-echo "Starting asgi server."
-asgi --chdir=/games/src/website \
-    --module=games.asgi:application \
-    --master --pidfile=/tmp/project-master.pid \
-    --socket=:8000 \
+echo "Starting uvicorn server."
+uvicorn --root-path=/games/src/website \
+    --port=:8000 \
     --processes=5 \
     --uid=www-data --gid=www-data \
-    --harakiri=20 \
-    --post-buffering=16384 \
-    --max-requests=5000 \
-    --thunder-lock \
-    --vacuum \
-    --logfile-chown \
-    --logto2=/games/log/asgi.log \
-    --ignore-sigpipe \
-    --ignore-write-errors \
-    --disable-write-exception
+    --limit-concurrency=5000 \
+    games.asgi:application
